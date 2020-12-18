b11f40779e81ee5549bc0419d40fb21350d834e7
jimb6 <jimwellbuot@gmail.com>
2020-12-18 19:32:40 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,6 @@ use Spatie\Permission\Models\Permission;
 |
 */
 
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
-header('Access-Control-Allow-Origin: *');
-
 //Auth::routes();
 Route::post('v1/agent/login', [\App\Http\Controllers\Auth\LoginController::class, 'loginAgent'])->name('agent.login');
 
