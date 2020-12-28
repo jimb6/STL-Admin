3207caa6808abb39a56a992237f57d9c9c84d002
Jim <jimwellbuot@gmail.com>
2020-12-28 21:58:28 +0800

    Revert "update app service providers for cors"
    This reverts commit 1b106f4949f95d8fff435c2749e949e9c82cc3db.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,8 +16,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Fruitcake\Cors\HandleCors::class,
         // \App\Http\Middleware\TrustHosts::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
@@ -35,7 +35,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-             \Illuminate\Session\Middleware\AuthenticateSession::class,
+//             \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
