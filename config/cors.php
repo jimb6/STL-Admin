af2ed2d3e66e18ad3c30fe730349ad3cd629fe08
Jim <jimwellbuot@gmail.com>
2020-12-28 22:04:28 +0800

    Revert "add allow origins"
    This reverts commit ce213b87c50a192d6f0c8da00b44aca58491babd.

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['file:///C:/Users/RSY-JIM/OneDrive/Documents/index.html', "*"],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
